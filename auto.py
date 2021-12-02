6676498d3f44fb665b636f373c0bf0019233790f
Humberto Suriano Medina <humbertosuriano24@gmail.com>
2021-12-02 09:43:30 -0600

    finish program

--- auto.py
+++ auto.py
@@ -1,19 +1,25 @@
 import threading
 import random
 import time
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
 
 class Auto(threading.Thread):
-    def __init__(self, piloto, pos):
+    def __init__(self, piloto, pos, vueltas,label, ganador):
         threading.Thread.__init__(self)
         self.estado = ''
-        self.vuelta = 0
+        self.vuelta = 1
         self.piloto = piloto
         self.pos = pos
+        self.label = label
+        self.ganador = ganador
+        self.vuelt = vueltas
     
     def vueltas(self, band):
 
-        aleatorioContador = 0    
-        aleatorioVuelta = random.choice([5,10,15,20])
+        aleatorioContador = 1    
+        aleatorioVuelta = random.choice([10,20,30,50])
         aleatorioTiempo = random.choice([0.02,0.01,0.03])
 
         while aleatorioContador <=  aleatorioVuelta:
@@ -23,8 +29,11 @@ class Auto(threading.Thread):
             if (aleatorioVuelta == aleatorioContador and band == False):
                 self.vuelta = self.vuelta + 1
                 self.estado = 'En pista'
+                self.label.setText(f'Estado: {self.estado}')
+
               
         if band == True:
+            
             self.pits()      
             self.band = False
                 
@@ -33,41 +42,31 @@ class Auto(threading.Thread):
         aleatorioTiempo = random.choice([1.8,2,2.5,3])
         time.sleep(aleatorioTiempo)
         self.estado = 'En pits'
+        self.label.setText(f'Estado: {self.estado}')
 
     def run(self):
         global win
         win = False
         pilotos = [['Checo', False], ['Mazepin', False], ['Shumager', False]]
         aleatorio = []
-        while self.vuelta <= 5 and win == False:
+        while True:
+
+            if win == True:
+                print('entre')
+                break
+
             aleatorio = random.choice(pilotos)
             for j in range(len(pilotos)):
                 if aleatorio == pilotos[j]:
                     pilotos[j][1] = True          
             self.vueltas(pilotos[self.pos][1])
             
-            if self.vuelta == 5:
+            self.vuelt.setText(f'Vueltas: {self.vuelta}')
+
+            if self.vuelta == 15:
                 win = True
+                self.ganador.setText(f'El ganador es {self.piloto}')
                 print('El ganador es: ', self.piloto)
-
+                break
+            
             pilotos = [['Checo', False], ['Mazepin', False], ['Shumager', False]]
-
-        
-
-if __name__ == '__main__':
-    pilotos = [['Checo', False], ['Mazepin', False], ['Shumager', False]]
-    
-    carro1 = Auto(pilotos[0][0], 0)
-    carro2 = Auto(pilotos[1][0], 1)
-    carro3 = Auto(pilotos[2][0], 2)
-    print(carro1.is_alive())
-    carro1.start()
-    carro2.start()
-    carro3.start() 
-    
-    print(carro1.is_alive())
-    
-    carro1.join()
-    carro2.join()
-    carro3.join()
-        
--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'designerAJeFWH.ui'
-##
-## Created by: Qt User Interface Compiler version 5.15.2
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
 import sys
 import platform
 from PySide6.QtCore import *
@@ -23,14 +13,24 @@ class mainWindow(QMainWindow):
         QMainWindow.__init__(self)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.pilotos = [['Checo', True], ['Mazepin', True], ['Shumager', True]]
+        self.pilotos = [['Checo', False], ['Mazepin', False], ['Shumager', False]]
+        self.carro1 = Auto(self.pilotos[0][0], 0, self.ui.label_7 ,self.ui.label, self.ui.ganador1)
+        self.carro2 = Auto(self.pilotos[1][0], 1, self.ui.label_8, self.ui.label_2, self.ui.ganador2)
+        self.carro3 = Auto(self.pilotos[2][0], 2, self.ui.label_9, self.ui.label_3, self.ui.ganador3)
+        self.ui.pushButton.clicked.connect(self.comenzar)
+        self.ui.pushButton_2.clicked.connect(self.reiniciar)
 
-        self.show()
+    def comenzar(self):
+        self.carro1.start()
+        self.carro2.start()
+        self.carro3.start()
 
-    
+    def reiniciar(self):
+        self.ui.pushButton_2.setEnabled(True)    
 
 if __name__ == '__main__':
     app =QApplication(sys.argv)
     window = mainWindow()
+    window.show()
     sys.exit(app.exec())
 
